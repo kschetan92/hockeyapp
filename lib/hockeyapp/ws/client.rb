b08e8d4fd343fe8e57cde303f479c5da229b20ed
Chetan K S <chetan.ks@v2solutions.com>
2016-02-11 17:48:08 +0530

    Update client.rb

--- lib/hockeyapp/ws/client.rb
+++ lib/hockeyapp/ws/client.rb
@@ -69,8 +69,8 @@ module HockeyApp
       App.from_hash(resp, self)
     end
     
-    def new_app(title, bundle_identifier, platform)
-      resp = ws.create_new_app(title, bundle_identifier, platform)
+    def new_app(params = {})
+      resp = ws.create_new_app(params)
       raise resp['errors'].map{|e|e.to_s}.join("\n") unless resp['errors'].nil?
       App.from_hash(resp, self)
     end
