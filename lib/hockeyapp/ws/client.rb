65c0d711d362a5418f0d023c806cecc52f138fb0
chetan K S <chetan.ks@v2solutions.com>
2016-02-26 12:04:22 +0530

    updating client

--- lib/hockeyapp/ws/client.rb
+++ lib/hockeyapp/ws/client.rb
@@ -72,14 +72,6 @@ module HockeyApp
     end
     
     def new_app title, bundle_id, options = {}
-      options[:custom_release_type] ||= ""
-      options[:owner_id] ||= ""
-      icon_path = options[:icon] ||= ""
-      unless icon_path == ""
-        accepted_formats = [".png", ".gif", ".jpeg"]
-        raise "Image format with #{File.extname(icon_path)} extension not supported" unless accepted_formats.include? File.extname(icon_path)
-        options[:icon] = File.open(icon_path, "rb")
-      end
       resp = ws.create_new_app(title, bundle_id, options)
       raise resp['errors'].map{|e|e.to_s}.join("\n") unless resp['errors'].nil?
       App.from_hash(resp, self)
